8275533cf98c8c4be37e758097aa70df7d967bc6
Jun-Yoon1 <jun.yn.work@gmail.com>
2025-12-17 06:28:39 +0000

    feat: summing up 2 numbers

---
+++ adder.py
@@ -0,0 +1,10 @@
+def adder(a: int, b: int) -> int:
+    """
+    Return the sum of two numbers.
+
+    :param a: first number
+    :param b: second number
+    :return: sum of a and b
+    """
+    return a + b
+
